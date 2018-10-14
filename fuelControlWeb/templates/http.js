fc58c7090fa2e33421b5cf66b6a9a8137cd48ebe
Pedro Honda <pedro.koh@gmail.com>
2018-10-14 12:43:00 -0300

    2018-10-14 : updates to fuelControlWeb's templates

---
+++ fuelControlWeb/templates/http.js
@@ -0,0 +1,23 @@
+function httpGET(url, callback) {
+    const http = new XMLHttpRequest();
+    http.open("GET", url, true);
+    http.onreadystatechange=(e)=>{
+        if (http.readyState == 4 && http.status == 200)
+            callback(http.responseText);
+    }
+    http.send();
+}
+
+function inputToParagraph(msg) {
+    document.getElementById("Paragraph").innerHTML = msg;
+}
+
+function httpGETparagraph(url) {
+    const http = new XMLHttpRequest();
+    http.open("GET", url, true);
+    http.onreadystatechange=(e)=>{
+        if (http.readyState == 4 && http.status == 200)
+            document.getElementById("Paragraph").innerHTML = http.responseText;
+    }
+    http.send();
+}
